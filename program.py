acef10b7c76f855334452cec2b3c3bd7cc016b1c
milign99 <miligonzaleznunez@gmail.com>
2024-10-29 21:41:13 -0300

    final

--- program.py
+++ program.py
@@ -33,10 +33,14 @@ def main():
         asiento, sala = seleccionarAsiento(sala)
         asientosSeleccionados.append(asiento)
             
-        otroAsiento = int(input("""¿Desea elegir otro asiento?\n1 - Si\n2 - No\nIngrese una opción: """))
-
-        while otroAsiento not in [1,2]:
+        exito = 0
+        while exito == 0:
             try:
+                otroAsiento = int(input("""¿Desea elegir otro asiento?\n1 - Si\n2 - No\nIngrese una opción: """))
+                
+                while otroAsiento not in [1,2]:
+                     otroAsiento = int(input("""Opcion incorrecta\n1 - Si\n2 - No\nIngrese una opción: """))
+                
                 while otroAsiento == 1:
                     asiento, sala = seleccionarAsiento(sala)
                     asientosSeleccionados.append(asiento)
@@ -45,21 +49,24 @@ def main():
                 if otroAsiento == 2:
                     print("""Por favor, retire su entrada y deje pasar al siguiente usuario\n¡Disfrute su película!""")
                     total_snacks = sum(snack[1] for snack in snacksSeleccionados)  
-                    total_entradas = PRECIO_ENTRADA * len(asientosSeleccionados)  
+                    total_entradas = PRECIO_ENTRADA * len(asientosSeleccionados)
+                
+                exito = 1
+
             except ValueError:
                     print("Entrada no válida. Por favor, ingresa un número.")
-                    otroAsiento = int(input("""¿Desea elegir otro asiento?\n1 - Si\n2 - No\nIngrese una opción: """))        
+         
 
-            calcular_total = lambda entradas, snacks, desc: (entradas + snacks) * (1 - desc)
-            calcular_total_sin_descuento = lambda entradas, snacks: entradas + snacks
+        calcular_total = lambda entradas, snacks, desc: (entradas + snacks) * (1 - desc)
+        calcular_total_sin_descuento = lambda entradas, snacks: entradas + snacks
                 
-            if descuento > 0:
-                total_a_pagar = calcular_total(total_entradas, total_snacks, descuento)
-                print(f"Total con descuento aplicado: ${total_a_pagar:.2f}")
-            elif descuento == 0:
-                total_a_pagar = calcular_total_sin_descuento(total_entradas, total_snacks)
-                print(f"Total sin descuento: ${total_a_pagar:.2f}")
-            imprimirTicket(pelicula, asientosSeleccionados, snacksSeleccionados, total_entradas, total_snacks, total_a_pagar, recaudacion, horario)
+        if descuento > 0:
+            total_a_pagar = calcular_total(total_entradas, total_snacks, descuento)
+            print(f"Total con descuento aplicado: ${total_a_pagar:.2f}")
+        elif descuento == 0:
+            total_a_pagar = calcular_total_sin_descuento(total_entradas, total_snacks)
+            print(f"Total sin descuento: ${total_a_pagar:.2f}")
+        imprimirTicket(pelicula, asientosSeleccionados, snacksSeleccionados, total_entradas, total_snacks, total_a_pagar, recaudacion, horario)
  
 
 if __name__ == "__main__":
